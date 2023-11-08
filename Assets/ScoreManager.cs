38d1127be43838c4f352622573794d18572793e4
Sh0w3r15 <tylerwen006@gmail.com>
2023-11-08 01:03:25 -0500

    Added highscore

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -6,16 +6,28 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     public Text scoreText;
+    public Text highscoreText;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        highscoreText.text = $"Highscore: {PlayerPrefs.GetInt("Highscore",0)}";
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = SpawnFood.BurgerCount.ToString();
+        CheckHighScore();
+    }
+
+    private void CheckHighScore()
+    {
+        if (SpawnFood.BurgerCount > PlayerPrefs.GetInt("Highscore", 0))
+        {
+            PlayerPrefs.SetInt("Highscore", SpawnFood.BurgerCount);
+            highscoreText.text = "Highscore: " + SpawnFood.BurgerCount.ToString();
+            Debug.Log(SpawnFood.BurgerCount);
+        }
     }
 }
